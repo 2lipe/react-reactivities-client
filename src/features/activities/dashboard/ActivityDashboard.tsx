2de260964242998aca32d6dce82a79e497713392
Felipe Vieira <felipe.svfx@gmail.com>
2021-05-01 20:36:44 -0300

    feat: create ActivityForm

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import { Grid } from 'semantic-ui-react';
 
 import { Activity } from '../../../app/models/activity';
 import { ActivityDetails } from '../details/ActivityDetails';
+import { ActivityForm } from '../form/ActivityForm';
 import { ActivityList } from './ActivityList';
 
 interface ActivityDashboardProps {
@@ -18,6 +19,7 @@ export const ActivityDashboard = ({ activities }: ActivityDashboardProps) => {
 
       <Grid.Column width="6">
         {activities[0] && <ActivityDetails activity={activities[0]} />}
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   );
